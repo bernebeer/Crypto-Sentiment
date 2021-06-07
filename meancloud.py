3a5c46770c0f4d16ebee16ac98d2d805c1ef9383
Gijs van Berne <gijs@bernebeer.nl>
2021-06-07 19:56:26 +0200

    Add files via upload

--- meancloud.py
+++ meancloud.py
@@ -15,8 +15,8 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 cryptolists = [1393929796825067522, 859922654681346049, 904733535612821504, 899718321439944710]
 	    
 alltweets = []
-numpages = 1
-numtweets = 2
+numpages = 4
+numtweets = 200
 
 # get all tweets by list, combine lists of tweets into 1 list
 # get tweets per page in list
@@ -33,6 +33,8 @@ for cryptolist in cryptolists:
 	alltweets.extend(tweets)
 	
 	print('List ' + str(cryptolists.index(cryptolist) + 1) + ' fetched!')
+
+print('----------')
 
 # sort full list by desc id
 alltweets = sorted(alltweets, key=lambda x: x.id, reverse=True)
@@ -57,7 +59,7 @@ print('Newest tweet: ', uniquetweets[0].created_at)
 
 print('------------')
 
-symbols = ['$BTC', '$ETH', '$DOGE', '$XRP', '$LTC', '$BCH', '$EOS', '$ADA']
+symbols = ['$BTC', '$ETH', '$DOGE', '$XRP', '$LTC', '$BCH', '$EOS', '$ADA', '$ETC', '$DASH', '$USDT']
 
 db = TinyDB('db.json')
 db.truncate()
@@ -69,20 +71,63 @@ cg = CoinGeckoAPI()
 cglist = cg.get_coins_list()
 cgfilt = []
 
+# get all cgids by symbol and add to dict in cgfilt list
+print('All cgids found by symbol:')
 for item in cglist:
-	#print(item.get('id'))
 	for symbol in symbols:
 		if symbol[1:].lower() == item.get('symbol') and 'peg' not in item.get('id'):
 			print(symbol,item.get('id'))
 			cgid = item.get('id')
-			cgfilt.append({'symbol':symbol, 'cgid':cgid})
+			cgfilt.append({'symbol':symbol, 'cgid':cgid, 'symbolmentions':0, 'avgsentiment':0, 'tweets':[]})
+			
+print('----------')
 
+# get price for each cgid in cgfilt list, update dict with price
 for item in cgfilt:
-	price = cg.get_price(ids=item['cgid'], vs_currencies='usd')
+	price = cg.get_price(ids=item['cgid'], vs_currencies='usd', include_24hr_change='true')
 	item.update({'price':price[item['cgid']]['usd']})
-	
-print(cgfilt)
+	item.update({'24hr':price[item['cgid']]['usd_24h_change']})
 
+# check if cgfilt symbol in uniquetweets, record num mentions, avg sentiment
+print('Tweets evaluated per symbol mention:')
+for uniquetweet in uniquetweets:
+	text = re.sub(r'@\S+|https?://\S+', '', uniquetweet.full_text)
+	text = re.sub('\n', ' ', text)
+	
+	for item in cgfilt:
+		if item['symbol'] in text:
+			mentioncount = item['symbolmentions'] + 1
+			item.update({'symbolmentions':mentioncount})
+			#print('found')
+			
+			payload={
+	   	 	'key': MCKEY,
+	    		'lang': 'en',
+	    		'txt': text
+			}
+			sentiments = requests.post(url, data=payload)
+			data = sentiments.json()
+			score = data['score_tag']
+			
+			avgsentiment = item['avgsentiment']
+			if score == 'P+':
+				avgsentiment += 2
+			elif score == 'P':
+				avgsentiment += 1
+			elif score == 'N':
+				avgsentiment -= 1
+			elif score == 'N+':
+				avgsentiment -= 2
+			
+			item.update({'avgsentiment':avgsentiment})
+			print('Mention', item['symbol'], 'found and evaluated!')
+			item['tweets'].append(text)
+			
+print('----------')
+
+print(json.dumps(cgfilt, indent=4, sort_keys=True))
+
+# find tweets containing symbols
 for uniquetweet in uniquetweets:
 	text = re.sub(r'@\S+|https?://\S+', '', uniquetweet.full_text)
 	text = re.sub('\n', ' ', text)
@@ -97,33 +142,33 @@ for uniquetweet in uniquetweets:
 		sep = (',')
 		#jsymbols = sep.join(fsymbol)
 	
-		print('Symbols found in tweet: ', fsymbol)
-		print('Text: ', text)
+		#print('Symbols found in tweet: ', fsymbol)
+		#print('Text: ', text)
 		
-		payload={
-	    	'key': MCKEY,
-	    	'lang': 'en',
-	    	'txt': text
-		}
-		sentiments = requests.post(url, data=payload)
+		#payload={
+	    	#'key': MCKEY,
+	    	#'lang': 'en',
+	    	#'txt': text
+		#}
+		#sentiments = requests.post(url, data=payload)
 		
-		data = sentiments.json()
-		score = data['score_tag']
-		print('Sentiment: ', score)
+		#data = sentiments.json()
+		#score = data['score_tag']
+		#print('Sentiment: ', score)
 		
-		db.insert({'id':uniquetweet.id, 'text':text, 'score':score, 'date':str(uniquetweet.created_at), 'symbols':fsymbol})
+		#db.insert({'id':uniquetweet.id, 'text':text, 'score':score, 'date':str(uniquetweet.created_at), 'symbols':fsymbol})
 		
-		ftweets.append(uniquetweet)
+		#ftweets.append(uniquetweet)
 		
-		print('Remaining credits: ', data['status']['remaining_credits'])
+		#print('Remaining credits: ', data['status']['remaining_credits'])
 		
-		print('------------')
+		#print('------------')
 
-print('Number of filtered tweets: ', len(ftweets))
+#print('Number of analysed tweets: ', len(uniquetweets))
 
 print('------------')
 
-with open('db.json', 'r') as handle:
-	parsed = json.load(handle)
+#with open('db.json', 'r') as handle:
+	#parsed = json.load(handle)
 
-print(json.dumps(parsed, indent=4, sort_keys=True))
+#print(json.dumps(parsed, indent=4, sort_keys=True))
